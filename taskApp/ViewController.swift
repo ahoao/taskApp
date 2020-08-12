9036ea295d8f3a5a66a65265fbaf4750589f9dc3
Ao.Nagasawa <aho_ao1004@yahoo.co.jp>
2020-08-12 16:38:26 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by Ao Nagasawa on 2020/08/12.
+//  Copyright © 2020 Ao.Nagasawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
